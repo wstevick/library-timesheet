e829520bb365c7c6fe7e007ecb345517c230e0ae
wstevick <12481632w@gmail.com>
2023-04-19 12:36:20 -0400

    style changes, refactor templates, and add nessecary information. I hope to add 2 weeks per page, but it's mostly finished for now

--- main.js
+++ main.js
@@ -12,28 +12,13 @@ document.getElementById('print').onclick = function(e) {
     const date = new Date(document.getElementById('start-date').valueAsDate.valueOf());
     date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
 
-    const template = document.getElementById('table-template').content;
-
     const newwin = window.open('./timesheet-template.html');
+    if (newwin === null) {
+        alert('Please allow popups for this website.');
+        return;
+    }
     newwin.onload = function() {
-        for (let period = 1; period <= 2; period++) {
-            const row = newwin.document.getElementById(`period-${period}`);
-
-            for (let i = 0; i < names.length; i++) {
-                const newelm = template.cloneNode(true);
-                newelm.querySelector('.employee-name').textContent = names[i];
-                row.appendChild(newelm);
-            }
-
-            for (let i = 0; i < 7; i++) {
-                const tables = row.querySelectorAll('.hours-body');
-                for (let j = 0; j < tables.length; j++) {
-                    const newelm = document.createElement('tr');
-                    newelm.innerHTML = `<td>${date.toLocaleDateString('en-US', { weekday: 'short' })} ${date.toLocaleDateString('en-US')}</td><td></td>`
-                    tables[j].appendChild(newelm);
-                }
-                date.incDays(1);
-            }
-        }
+        newwin.makeTimesheets(date, names);
+        newwin.print();
     };
 }
---
+++ timesheets.js
@@ -0,0 +1,32 @@
+window.makeTimesheets = function(date, names) {
+    const tableTemplate = document.getElementById('table-template').innerHTML;
+
+    const week1 = document.createElement('div');
+    week1.classList.add('col');
+    const week2 = week1.cloneNode(true);
+
+    week1.innerHTML = tableTemplate + document.getElementById('week-1-things').innerHTML;
+    week2.innerHTML = tableTemplate + document.getElementById('week-2-things').innerHTML;
+
+    week1.querySelector('.week-num').textContent = '1';
+    week2.querySelector('.week-num').textContent = '2';
+
+    for (let i = 1; i <= 2; i++) {
+        const week = i === 1 ? week1 : week2;
+
+        for (let j = 0; j < 7; j++) {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `<td>${date.toLocaleDateString('en-US', { weekday: 'short' })} ${date.getMonth()}/${date.getDate()}</td><td></td>`;
+            week.querySelector('.hours-body').appendChild(tr);
+            date.setDate(date.getDate() + 1);
+        }
+    }
+
+    for (let i = 0; i < names.length; i++) {
+        for (let j = 1; j <= 2; j++) {
+            const table = (j === 1 ? week1 : week2).cloneNode(true);
+            table.querySelector('.employee-name').textContent = names[i];
+            document.getElementById(`week-${j}`).appendChild(table);
+        }
+    }
+};
