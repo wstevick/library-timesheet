e309f5bfd4592edbaac5443d6ceca843c7a75f63
wstevick <107445557+wstevick@users.noreply.github.com>
2023-04-19 16:04:20 -0400

    Add verification if the day entered isn't a Friday.

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ document.getElementById('print').onclick = function(e) {
 
     const date = new Date(document.getElementById('start-date').valueAsDate.valueOf());
     date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+    
+    if (date.getDay() !== 6 && !confirm(`You entered a ${date.toLocaleDateString('en-US', { weekday: 'long' })}. Are you sure you want this?`) {
+        return;
+    }
 
     const newwin = window.open('./timesheet-template.html');
     if (newwin === null) {
